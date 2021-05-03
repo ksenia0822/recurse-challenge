48abbcf03ab8292af7cbf95dc5535502d8c37e5d
xenia <ksenia0822@gmail.com>
2021-05-02 19:24:45 -0700

    add get

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -3,10 +3,16 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class HomeController {
+    Map<String, String> cache;
+
+    public HomeController() {
+        cache = new HashMap<>();
+    }
 
     @RequestMapping("/hello/{name}")
     String hello(@PathVariable String name) {
@@ -15,7 +21,16 @@ public class HomeController {
 
     @RequestMapping("/set")
     String set(@RequestParam Map<String, String> allParams) {
-        return allParams.toString();
+        cache.putAll(allParams);
+        return String.format("Updated cache with the following values: %s", allParams.toString());
+    }
+
+    @RequestMapping("/get")
+    String get(@RequestParam Map<String, String> allParams) {
+        if (allParams.size() != 1) {
+            throw  new IllegalArgumentException("Cannot retrieve multuple keys");
+        }
+        return allParams.get(allParams.keySet().iterator().next());
     }
 }
 
