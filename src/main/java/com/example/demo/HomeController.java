d434e29c1bcd973f42ae1a99b94920b9072e79e2
xenia <ksenia0822@gmail.com>
2021-05-07 15:39:18 -0700

    pair

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -3,15 +3,19 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class HomeController {
+    private static String FILENAME = "storage.txt";
     Map<String, String> cache;
+    FileWriter writer;
 
-    public HomeController() {
+    public HomeController() throws IOException {
         cache = new HashMap<>();
+        writer = new FileWriter(FILENAME, false);
     }
 
     @RequestMapping("/hello/{name}")
@@ -20,17 +24,34 @@ public class HomeController {
     }
 
     @RequestMapping("/set")
-    String set(@RequestParam Map<String, String> allParams) {
-        cache.putAll(allParams);
-        return String.format("Updated cache with the following values: %s", allParams.toString());
+    String set(@RequestParam Map<String, String> allParams) throws IOException {
+        // cache.putAll(allParams);
+        BufferedWriter currentWriter = new BufferedWriter(new FileWriter(FILENAME, true));
+        currentWriter.append("\n");
+        for (String key : allParams.keySet()) {
+            currentWriter.append(key + "=" + allParams.get(key));
+        }
+        currentWriter.close();
+        return String.format("Updated file with the following values: %s", allParams.toString());
     }
 
     @RequestMapping("/get")
-    String get(@RequestParam("key") String key) {
-        if (cache.isEmpty()) {
-            return "there is nothing in the cache";
+    String get(@RequestParam("key") String key)  throws IOException {
+        BufferedReader input = new BufferedReader(new FileReader(FILENAME));
+        String last = null;
+        String line;
+
+        while ((line = input.readLine()) != null) {
+            String[] split = line.split("=");
+            if (split[0].equals(key)) {
+                last = line;
+            }
         }
-        return cache.get(key);
+
+        if (last == null) {
+            return "No entry found";
+        }
+        return "Last entry was " + last;
     }
 }
 
